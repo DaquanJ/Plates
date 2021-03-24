7b500c3b7ef89fe7116c3519c9df69a4c44867c6
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-24 18:22:29 -0400

    added and removed divs

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,17 +5,19 @@ import '../styles/Nav.css';
 const Nav = () => {
     return (
         <div className="navigation" >
-            <h2> Plates </h2>
+
+            <h2>Plates</h2>
             <ul>
                 <li>Browse</li>
                 <li>Cart</li>
             </ul>
 
-            <h2> My Plates </h2>
+            <h2>My Plates</h2>
             <ul>
                 <li>All</li>
                 <li>Favorites</li>
             </ul>
+
         </div>
     );
 
