4a34626b716c72c89145db2eca1b78768dcff270
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-23 22:26:44 -0400

    generated constructor, getters and setters

--- server/src/main/java/com/example/server/model/Cart.java
+++ server/src/main/java/com/example/server/model/Cart.java
@@ -24,6 +24,45 @@ public class Cart {
     @Column
     private String price;
 
+    public Cart() { }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
