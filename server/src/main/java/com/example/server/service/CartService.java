962c636224ece4a93bad796fb33f83001c1fb543
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-23 22:51:20 -0400

    implented service for get,getbyid,create,update,delete

--- server/src/main/java/com/example/server/service/CartService.java
+++ server/src/main/java/com/example/server/service/CartService.java
@@ -1,7 +1,9 @@
 package com.example.server.service;
 
+import com.example.server.model.Cart;
 import com.example.server.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +12,25 @@ public class CartService {
     @Autowired
     private CartRepository cartRepository;
 
+    public Iterable<Cart> getItems() {
+        return cartRepository.findAll();
+    }
 
+    public Cart getItemById(Long id) {
+        return cartRepository.findById(id).get();
+    }
+
+    public Cart createItem(Cart item) {
+        return cartRepository.save(item);
+    }
+
+    public Cart updateItem(Cart item) {
+        return cartRepository.save(item);
+    }
+
+    public HttpStatus deleteItem(Long id) {
+        cartRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
 
 }
