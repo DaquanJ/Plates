96d105c6e1db55cbe19c189ec8bcd7b43b44fc51
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-23 22:41:08 -0400

    created interface repository, extending CrudRepo

---
+++ server/src/main/java/com/example/server/repository/CartRepository.java
@@ -0,0 +1,6 @@
+package com.example.server.repository;
+
+import com.example.server.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart,Long> {}
