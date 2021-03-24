16afc50c0e0894cf7051b412d593df514ecd66d4
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-23 22:58:21 -0400

    created controller class, annotated class as RestController, added base endpoint with requestmapping and implemented controller get,post,patch and delete mapping methods

---
+++ server/src/main/java/com/example/server/controller/CartController.java
@@ -0,0 +1,41 @@
+package com.example.server.controller;
+
+import com.example.server.model.Cart;
+import com.example.server.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    CartService cartService;
+
+    @GetMapping
+    public Iterable<Cart> getItems() {
+        return cartService.getItems();
+    }
+
+    @GetMapping("/{id}")
+    public Cart getItemById(@PathVariable Long id) {
+        return cartService.getItemById(id);
+    }
+
+    @PostMapping
+    public Cart createItem(@RequestBody Cart item) {
+        return cartService.createItem(item);
+    }
+
+    @PatchMapping
+    public Cart updateItem(@RequestBody Cart item) {
+        return cartService.updateItem(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteItem(@PathVariable Long id) {
+        return cartService.deleteItem(id);
+    }
+    
+}
