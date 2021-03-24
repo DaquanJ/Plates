964739374af9244bd929a52ae7f6f696b7a84be9
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-23 22:37:18 -0400

    annotated class as entity, added id, generated constructor, getters and setters

---
+++ server/src/main/java/com/example/server/model/Checkout.java
@@ -0,0 +1,89 @@
+package com.example.server.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="checkout")
+public class Checkout {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String address;
+
+    @Column
+    private String city;
+
+    @Column
+    private String phone;
+
+    public Checkout() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
