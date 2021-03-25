e871152c82666107bf2e4fea4605bf51a1b0e943
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-24 21:57:21 -0400

    imported react-router-dom components, implemented paths for browse and cart components using router switch and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Nav from './components/Nav';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Browse from './components/Browse';
+import Cart from './components/Cart';
+
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/browse' component={Browse} />
+          <Route path='/cart' component={Cart} />
+        </Switch>
+      </Router>
     </div>
   );
 }
