8fc442eb62d13827c125981a450eb107df2c4175
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-24 21:51:15 -0400

    established browse.jsx as functional component

---
+++ client/src/components/Browse.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Browse = () => {
+    return (
+        <div>
+            <h1>Browse Food </h1>
+        </div>
+    );
+}
+
+export default Browse;
