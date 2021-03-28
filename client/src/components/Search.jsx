b9d764b2af678dd0ce2d9ce3bc5c9917acbebf33
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-28 16:17:26 -0400

    successful request to api

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,12 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
+const apikey = process.env.REACT_APP_API_KEY;
+const appid = process.env.REACT_APP_API_ID;
+
 const Search = () => {
 
     const [food, setFood] = useState([]);
     const [search, setSearch] = useState({});
 
+    async function getFood() {
+        try {
+            const res = await axios.get(`https://api.edamam.com/search?q=${search.search}&app_id=${appid}&app_key=${apikey}`)
+            setFood(res.data.hits)
+            console.log(res.data.hits)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
+    useEffect(() => {
+        getFood()
+    }, [])
 
     return (
         <div>
