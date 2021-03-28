e92c8f24e9401bbd98144456a1a521de24028d17
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-28 15:58:19 -0400

    changed elem to input

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <input type="text" placeholder="search food" />
+        </div>
+    );
+}
+
+export default Search;
