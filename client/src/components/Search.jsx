fb6052189528d3197dc7a3bcd2d382a46e5509c7
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-03-28 16:01:11 -0400

    made references to state for data and user input using usestate hook

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
 const Search = () => {
+
+    const [food, setFood] = useState([]);
+    const [search, setSearch] = useState({});
+
+
+
     return (
         <div>
             <input type="text" placeholder="search food" />
