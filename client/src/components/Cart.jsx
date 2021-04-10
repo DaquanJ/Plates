547f0f0c782394f998fcb08617dd038311ebc846
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-04-10 16:57:53 -0400

    imported numeral to format price, useState hook used for cart state, successfully connected to my api , deleteItem function added to handle remove from cart/delete requests, useEffect hook used to run getcart function on load, mapped cart state and rendered items to dom

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import numeral from 'numeral';
 
 const Cart = () => {
+
+    const [cart, setCart] = useState([])
+
+    async function getCart() {
+        try {
+            const res = await axios.get('http://localhost:8080/cart/')
+            setCart(res.data)
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async function deleteItem(id) {
+        try {
+            const res = await axios.delete(`http://localhost:8080/cart/${id}`)
+            setCart([...cart].filter(cart => cart.id !== id))
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getCart()
+    }, [])
+
     return (
         <div>
-            <h1> Cart </h1>
+            {cart.map(item =>
+                <div>
+                    <h1> Your Order </h1>
+                    <img src={item.image} alt={item.name} />
+                    <h1> {item.name} </h1>
+                    <p> price: ${numeral(item.price).format('0.00')} </p>
+                    <button> {`Check Out - $${numeral(item.price).format('0.00')} `} </button>
+                    <button onClick={() => deleteItem(item.id)} > {`Remove ${item.name}`} </button>
+                </div>
+            )}
         </div>
     );
 }
