72facbe1bc523fb108fc3ea87bd7d575556d8d0e
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-04-10 20:18:53 -0400

    brought in useState for browse comp and useEffect running getPlates on mount, getPlates function established to make call for food data

--- client/src/components/Browse.jsx
+++ client/src/components/Browse.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const apikey = process.env.REACT_APP_API_KEY;
+const appid = process.env.REACT_APP_API_ID;
 
 const Browse = () => {
+
+    const [browse, setBrowse] = useState([])
+
+    async function getPlates() {
+        try {
+            const res = await axios.get(`https://api.edamam.com/search?q=food&app_id=${appid}&app_key=${apikey}&to=30`)
+            setBrowse(res.data.hits)
+            console.log(res.data.hits)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getPlates()
+    }, [])
+
     return (
         <div>
             <h1>Browse Food </h1>
