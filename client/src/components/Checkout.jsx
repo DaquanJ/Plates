f31372ed0c4fb9a05610a92d96f47121724139ac
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-04-10 20:22:11 -0400

    imported axios, est state for checkout comp with useState hook, handleChange function added to grab input from checkout form, dynamically setting object to state, submitCheckout function added to handle post request to backend of checkout form, form with inputs and submit rendered

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,9 +1,41 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Checkout = () => {
+
+    const [checkout, setCheckout] = useState({})
+
+    function handleChange(e) {
+        const { id, value } = e.target;
+        setCheckout({ ...checkout, [id]: value })
+        console.log(checkout)
+    }
+
+    async function submitCheckout(e) {
+        e.preventDefault();
+        try {
+            const res = await axios.post('http://localhost:8080/checkout', checkout)
+            console.log(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+    }, [])
+
     return (
         <div>
-            <h1>Checkout</h1>
+
+            <form onSubmit={(e) => submitCheckout(e)} onChange={(e) => handleChange(e)}>
+                <input type="text" id='email' placeholder='Email' />
+                <input type="text" id='firstName' placeholder='First Name' />
+                <input type="text" id='lastName' placeholder='Last Name' />
+                <input type="text" id='address' placeholder='Address' />
+                <input type="text" id='city' placeholder='City' />
+                <input type="text" id='phone' placeholder='Phone' />
+                <input type="submit" value='Continue To Payment' />
+            </form>
         </div>
     );
 }
