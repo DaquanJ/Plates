befc820c8d265e662174d55e7a8c3ddf3a11b91f
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-02 18:41:28 -0400

    added link to submit input for payment comp, imported link from react-router-dom

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Checkout = () => {
 
@@ -33,7 +34,7 @@ const Checkout = () => {
                 <input type="text" id='address' placeholder='Address' />
                 <input type="text" id='city' placeholder='City' />
                 <input type="text" id='phone' placeholder='Phone' />
-                <input type="submit" value='Continue To Payment' />
+                <Link to='/payment' > <input type="submit" value='Continue To Payment' /></Link>
             </form>
         </div>
     );
