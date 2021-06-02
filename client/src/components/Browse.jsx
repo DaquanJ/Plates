e206903a0f1cd13e4cf8cb07cc31464f82f9be9c
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-02 18:17:28 -0400

    altered browse component to be static, only renders components making requests for food types

--- client/src/components/Browse.jsx
+++ client/src/components/Browse.jsx
@@ -1,31 +1,16 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
 
-const apikey = process.env.REACT_APP_API_KEY;
-const appid = process.env.REACT_APP_API_ID;
+import Pasta from './Pasta';
+import Seafood from './Seafood';
+import Steak from './Steaks';
+
+
 
 const Browse = () => {
-
-    const [browse, setBrowse] = useState([])
-
-    async function getPlates(plate) {
-        try {
-            const res = await axios.get(`https://api.edamam.com/search?q=${plate}&app_id=${appid}&app_key=${apikey}&to=10`)
-            setBrowse(res.data.hits)
-            console.log(res.data.hits)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    useEffect(() => {
-        getPlates('chicken')
-        getPlates('beef')
-    }, [])
-
     return (
         <div>
-            <h1>Browse Food </h1>
+            <Pasta />
+            <Seafood />
+            <Steak />
         </div>
     );
 }
